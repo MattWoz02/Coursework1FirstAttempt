4cac36ba6d9d47cacb515ca522676e26fa58b6ce
MattWoz02 <mattwozniak02@gmail.com>
2023-11-12 21:44:59 +0000

    Return an error if no input argument is provided

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -6,7 +6,12 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {  
+    {
+	if (args.lenth == 0)
+	{
+	   System.out.println("Error: No input argument provided. Please provide a valid integer.");
+	   return;
+	}
 
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
