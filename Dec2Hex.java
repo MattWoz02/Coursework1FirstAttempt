c9fc0ddc782b306f1975f69dcc46082a52336455
MattWoz02 <mattwozniak02@gmail.com>
2023-11-12 17:39:45 +0000

    remove Jenkins test changes

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -6,7 +6,7 @@ class Dec2Hex
     public static int Arg1;
 
     public static void main(String args[])
-    {  System.out.println("Test Jenkins to detect changes");
+    {  
 
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
