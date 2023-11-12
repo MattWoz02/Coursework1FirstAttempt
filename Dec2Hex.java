bfd9ee24d3447ca4835d808a9511930d8e4a8df0
MattWoz02 <mattwozniak02@gmail.com>
2023-11-12 23:46:24 +0000

    Handle Negative Numbers

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -13,6 +13,11 @@ class Dec2Hex
 	   return;
 	}
 
+	if (Arg1 < 0) {
+                System.out.println("Error: Negative integers are not supported. Please provide a non-negative integer number.");
+                return;
+            }
+
 	try{
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
