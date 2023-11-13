c39edb945d7a9496df00e43129cc6cbc90d6e229
MattWoz02 <mattwozniak02@gmail.com>
2023-11-13 13:45:30 +0000

    Modify Test Runner

--- TestRunner.java
+++ TestRunner.java
@@ -1,27 +1,23 @@
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
-
 import org.junit.runner.notification.Failure;
-import java.lang.Exception;
 
 public class TestRunner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(Dec2HexTest.class);
-      int fails = 0;
-      try{
-          for (Failure failure : result.getFailures()) {
-             System.out.println(failure.toString());
-             fails++;
-         }
-          if(fails >0)
-            throw  new Exception();
-          System.out.println(result.wasSuccessful());
-      }
-      catch(Exception e)
-      {
-          System.out.println("" + fails + " tests failed");
-          System.exit(1);
-      }
-      System.exit(0);
-   }
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Dec2HexTest.class);
+
+        int fails = result.getFailureCount();
+        
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        if (fails > 0) {
+            System.out.println("Tests failed: " + fails);
+            System.exit(1); // Non-zero exit code indicates an error
+        } else {
+            System.out.println("All tests passed successfully.");
+            System.exit(0); // Zero exit code indicates success
+        }
+    }
 }
