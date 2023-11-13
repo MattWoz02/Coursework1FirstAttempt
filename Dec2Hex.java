82062febddea8e5088a9479a1f8b547331787d71
MattWoz02 <mattwozniak02@gmail.com>
2023-11-13 01:19:25 +0000

    Check For Negative Values Move IF Statement

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -13,13 +13,14 @@ class Dec2Hex
 	   return;
 	}
 
-	if (Arg1 < 0) {
-                System.out.println("Error: Negative integers are not supported. Please provide a non-negative integer number.");
+	try{
+        Arg1 = Integer.parseInt(args[0]);
+
+if (Arg1 < 0) {
+                System.out.println("Error: Negative integers are not supported. P>
                 return;
             }
 
-	try{
-        Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
